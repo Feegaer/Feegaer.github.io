3654e9e3b5eb4ae6d2c5a8ac2ce3b851e51ceb95
Feegaer <lanevecristianezequiel@gmail.com>
2025-08-08 00:35:27 -0300

    Added CV in english and spanish and a base profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import CardList from './components/CardList/CardList'
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons";
+
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <CardList 
-        cards={[
-          { id: '1', title: 'Card 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula quam non mi commodo, ut commodo ex gravida. Sed lacinia vitae purus laoreet imperdiet. Fusce maximus consequat velit, vitae viverra lorem fringilla semper. Nunc id massa id ligula mollis tristique nec vel massa. Vivamus congue convallis iaculis. Fusce id nibh vulputate felis pretium laoreet. Fusce hendrerit id magna et luctus. Mauris et turpis bibendum, vestibulum leo a, laoreet nisi.' },
-          { id: '2', title: 'Card 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula quam non mi commodo, ut commodo ex gravida. Sed lacinia vitae purus laoreet imperdiet. Fusce maximus consequat velit, vitae viverra lorem fringilla semper. Nunc id massa id ligula mollis tristique nec vel massa. Vivamus congue convallis iaculis. Fusce id nibh vulputate felis pretium laoreet. Fusce hendrerit id magna et luctus. Mauris et turpis bibendum, vestibulum leo a, laoreet nisi.' },
-          { id: '3', title: 'Card 3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula quam non mi commodo, ut commodo ex gravida. Sed lacinia vitae purus laoreet imperdiet. Fusce maximus consequat velit, vitae viverra lorem fringilla semper. Nunc id massa id ligula mollis tristique nec vel massa. Vivamus congue convallis iaculis. Fusce id nibh vulputate felis pretium laoreet. Fusce hendrerit id magna et luctus. Mauris et turpis bibendum, vestibulum leo a, laoreet nisi.' }
-        ]}
-      />
+    <>
+    <div className="grid gap-4 p-4">
+      <div className="w-98 h-98 flex">
+        <img 
+          width="100%"
+          height="100%"
+          className="object-cover border border-gray-600"
+          src="" 
+        />
+      </div>
+      <div>
+        <header className="text-2xl uppercase">Cristian Ezequiel La Neve</header>
+        <p className="text-sm uppercase mb-3">Fullstack Developer</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt assumenda distinctio obcaecati repellat officiis magnam vero. Velit quis blanditiis minima temporibus alias, sunt at inventore ipsam ipsa sint, non pariatur!</p>
+      </div>
+      <a className="border p-2" href="/src/assets/CV_ES_CRISTIAN_EZEQUIEL_LA_NEVE.pdf" target="_blank">
+        <FontAwesomeIcon icon={faFilePdf} />
+        <span className="ms-2">Curriculum Vitae - Español</span>
+      </a>
+      <a className="border p-2" href="/src/assets/CV_EN_CRISTIAN_EZEQUIEL_LA_NEVE.pdf" target="_blank">
+        <FontAwesomeIcon icon={faFilePdf} />
+        <span className="ms-2">Curriculum Vitae - English</span>
+      </a>
+      <a className="border p-2" href="https://www.linkedin.com/in/ezelnv/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedinIn} />
+        <span className="ms-2">LinkedIn</span>
+      </a>
     </div>
+    </>
   )
 }
 
-export default App
+export default App;
